e9d0f682f809680232f15d2deca246aa982c110e
kaname-dev <kaname.dev@gmail.com>
2025-02-08 21:55:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,46 +20,66 @@ document.addEventListener("DOMContentLoaded", function () {
   gtag("config", "G-5V00F9RQS5");
 })();
 
-// Instagramリンクボタンがクリックされたとき
-document.getElementById('instagram-toggle').addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'Instagram Toggle'
+// Instagramのトグルボタンのクリックイベントをトラッキング
+        gtag('event', 'click', {
+            'event_category': 'Social Links',
+            'event_label': 'Instagram Toggle'
+        });
     });
-});
 
 // LinkedInリンクボタンがクリックされたとき
-document.querySelectorAll('.social-link')[0].addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'LinkedIn'
+    document.querySelector('.social-link[href="https://linkedin.com/in/kaname-w-73799734a"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Social Links',
+            'event_label': 'LinkedIn'
+        });
     });
-});
 
-// YouTubeリンクボタンがクリックされたとき
-document.querySelectorAll('.social-link')[1].addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'YouTube'
+    // YouTubeリンクボタンがクリックされたとき
+    document.querySelector('.social-link[href="https://www.youtube.com/@RobotchallengebyH.S.student"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Social Links',
+            'event_label': 'YouTube'
+        });
     });
-});
 
-document.querySelectorAll('.social-link')[2].addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'Contact'
+    // Instagramの追加オプションリンク（coding）クリック
+    document.querySelector('.social-link[href="https://www.instagram.com/kay_codes"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Instagram Options',
+            'event_label': 'Instagram Coding'
+        });
     });
-});
-document.querySelectorAll('.social-link')[3].addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'GiHhub'
-    });
-});
-document.querySelectorAll('.social-link')[4].addEventListener('click', function() {
-    gtag('event', 'links', {
-        'link_total': 'Social Links',
-        'links': 'GiHhub Project'
-    });
-});
 
+    // Instagramの追加オプションリンク（private）クリック
+    document.querySelector('.social-link[href="https://www.instagram.com/kana.06x_x"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Instagram Options',
+            'event_label': 'Instagram Private'
+        });
+    });
+
+    // Contactリンクボタンがクリックされたとき
+    document.querySelector('.social-link[href="https://docs.google.com/forms/d/e/1FAIpQLSfdHYBuqtSKHc8w_M9laIi5jvYeuahvczxdw-ZBggr0nQp4Uw/viewform"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Social Links',
+            'event_label': 'Contact'
+        });
+    });
+
+    // GitHubリンクボタンがクリックされたとき
+    document.querySelector('.social-link[href="https://github.com/kanamecode?tab=repositories"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Projects',
+            'event_label': 'GitHub'
+        });
+    });
+
+    // プロジェクトのホームページリンクがクリックされたとき
+    document.querySelector('.social-link[href="https://kanamecode.github.io/Photo-Booth-System/"]').addEventListener('click', function() {
+        gtag('event', 'click', {
+            'event_category': 'Projects',
+            'event_label': 'Project Homepage'
+        });
+    });
+});
