d6a17f1e7b440c85f1f0ab1af40a937b780d6472
kaname-dev <kaname.dev@gmail.com>
2025-02-08 18:33:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+// analytics.js
+(function() {
+  var gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-5V00F9RQS5";
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+
+  gtag("js", new Date());
+  gtag("config", "G-5V00F9RQS5");
+})();
